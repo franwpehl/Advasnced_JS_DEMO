06363835feb3314bd3035b8bfbf970c3b204e8fb
franwpehl <73023614+franwpehl@users.noreply.github.com>
2020-12-13 11:24:03 -0600

    Add files via upload

--- scope.js
+++ scope.js
@@ -1,6 +1,21 @@
-let num = 5;
+$(document).ready(function () {
+    var friends = ["Vince", "Milan", "Curtis", "Alex", "Brock"];
+    var locations = ["Crankenstein", "Gus Fruh", "New Wall", "Insanity Wall", "Mossy Wall", "Awesome roof", "Monster Rock", "Med Wall", "Th Red", "HCR"];
+    var weapons = ["Mossberg 12 guage", "Glock 9mm", "Rope", "Machete", "AK-47", "TNT", "Light Saber", "Garrote", "Rocket Launcher", "Axe", "Bat", "Brass Knuckles", "whip", "mace", "Cat of 9", "Fugu", "shield", "shuriken", "dart", "knife"];
 
-function showSquare() {
-    let sq = num * num;
-    alert(sq);
-}
+    for (var i = 1; i <= 100; i++) {
+        var $h3 = $("<h3>Accusation " + i + "</h3>");
+        $($h3).appendTo("main");
+        $($h3).click(getAlert(i));
+    }
+
+    function getAlert(i) {
+        var friend = friends[i % 5];
+        var location = locations[i % 10];
+        var weapon = weapons[i % 20];
+        function accuAlert() {
+            alert("Accusation " + i + ": I accuse " + friend + " with the " + weapon + " in " + location + "!");
+        }
+        return accuAlert;
+    }
+});
